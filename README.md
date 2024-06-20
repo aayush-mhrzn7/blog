83384191eb8e0c17c7ebd1f96742c95590f5ea0a
Aayush <aayushmaharjan497@gmail.com>
2024-06-20 13:44:45 +0545

    configuration of blog

---
+++ README.md
@@ -0,0 +1,6 @@
+# Blog
+
+this is a react application that uses appwrite as a backend to store the file and details of the user as well as authentication
+it uses react router for routing and react-redux for state management
+
+--project by Aayush maharjan
---
+++ config/config.js
@@ -0,0 +1,6 @@
+const config = {
+  appwriteURl: String(import.meta.env.VITE_PROJECT_URL),
+  appwriteID: String(import.meta.env.VITE_PROJECT_ID),
+  databaseID: String(import.meta.env.VITE_DATABASE_ID),
+  collectionId: String(import.meta.env.VITE_COLLECTION_ID),
+};
