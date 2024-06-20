03e1ceb4215952b3677f8c219e49aa905d4bef5b
Aayush <aayushmaharjan497@gmail.com>
2024-06-20 16:42:09 +0545

    final config

--- appwrite/service.js
+++ appwrite/service.js
@@ -1,12 +1,14 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases, ID, Query, Storage } from "appwrite";
 import config from "../config/config";
 
 export class AppwriteService {
   client = new Client();
   database;
+  storage;
   constructor() {
     this.client.setEndpoint(config.appwriteURl).setProject(config.appwriteID);
     this.database = new Databases(this.client);
+    this.storage = new Storage(this.client);
   }
   //CRUD operations for creating a document
   async createPost({ title, content, slug, featuredImage, status, userId }) {
@@ -88,5 +90,38 @@ export class AppwriteService {
       console.log("error during listinf all documents", error);
     }
   }
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.database.listDocuments(
+        config.databaseID,
+        config.collectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("error getting multiple files");
+    }
+  }
   //file upload images
+  async createFile(file) {
+    try {
+      return await this.storage.createFile(config.bucketId, ID.unique(), file);
+    } catch (error) {
+      console.log("error in file creation", error);
+    }
+  }
+  async deleteFile(fileId) {
+    try {
+      await this.storage.deleteFile(config.bucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("deleted file err", error);
+      return false;
+    }
+  }
+  getfilePreview(fileId) {
+    return this.storage.getfilePreview(config.bucketId, fileId);
+  }
 }
+
+const app_service = new AppwriteService();
+export default app_service;
--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ const config = {
   appwriteID: String(import.meta.env.VITE_PROJECT_ID),
   databaseID: String(import.meta.env.VITE_DATABASE_ID),
   collectionId: String(import.meta.env.VITE_COLLECTION_ID),
+  bucketId: String(import.meta.env.VITE_BUCKET_ID),
 };
 export default config;
